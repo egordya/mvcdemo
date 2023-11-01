e120a8d0a3eb3ba4a5dda3131f528331fdca43a3
egordya <91674367+egordya@users.noreply.github.com>
2023-11-01 15:33:47 +0100

    Added two functions now, code it working.

--- src/com/company/mvcdemo/model.java
+++ src/com/company/mvcdemo/model.java
@@ -8,6 +8,13 @@ public class model {
         calculationValue = firstNumber + secondNumber;
     }
 
+    public void multiplyTwoNumbers(int firstNumber, int secondNumber){
+        calculationValue = firstNumber * secondNumber;
+    }
+
+    public void divideTwoNumbers(int firstNumber, int secondNumber){
+        calculationValue = firstNumber / secondNumber;
+    }
 
     public int getCalculationValue() {
         return calculationValue;
